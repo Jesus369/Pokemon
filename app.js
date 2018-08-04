22b28406f10f7ebe8ac52945c62085e6a646da65
Jesus <je.flores28@gmail.com>
2018-08-04 10:27:26 -0500

    committing

--- app.js
+++ app.js
@@ -165,16 +165,8 @@ app.put(
   dbPost.updatePokemon
 );
 
-app.set("port", process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000;
 
-app
-  .get("/", function(request, response) {
-    var result = "App is running";
-    response.send(result);
-  })
-  .listen(app.get("port"), function() {
-    console.log(
-      "App is running, server is listening on port ",
-      app.get("port")
-    );
-  });
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
