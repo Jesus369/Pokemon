58400e93c10533793bcafe9c923b54264f940548
Jesus <je.flores28@gmail.com>
2018-08-04 13:31:09 -0500

    commit

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ const pool = new Pool({
   connectionString: connectString
 });
 
+var env = process.env.NODE_ENV || `development`;
+var config = require(__dirname + `/../config/config.json’)[env]`);
+var db = {};
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
 app.use(
   session({
     store: sessionStore,
